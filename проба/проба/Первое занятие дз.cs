41c27d744509c837fb0202b884f7130799166de1
Blin4iki <vladdexb@gmail.com>
2024-03-19 19:29:50 +0300

    Добавьте файлы проекта.

---
+++ проба/проба/Первое занятие дз.cs
@@ -0,0 +1,30 @@
+﻿//Console.WriteLine("Введите Имя");
+//string name= Console.ReadLine();
+//Console.WriteLine("Введите Фамилию");
+//string surname = Console.ReadLine();
+//Console.WriteLine( surname);
+//Console.WriteLine( $"ваше имя {name}");
+
+//Console.WriteLine("Введите первое число");
+//int number1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите второе число");
+//int number2 = int.Parse(Console.ReadLine());
+//Console.WriteLine(number1 + number2);
+
+//Console.WriteLine("Введите первое число");
+//int number1 = int.Parse(Console.ReadLine());
+//Console.WriteLine($"{number1 /2 }");
+
+//Console.WriteLine("Мы имеем прямоугольный треугольник ABC");
+//Console.WriteLine("Надо найтигипотенузу BC");
+//Console.WriteLine("Введите AB");
+//int AB = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите AC");
+//int AC = int.Parse(Console.ReadLine());
+//Console.WriteLine($"{Math.Sqrt(AB * AB + AC * AC)}");
+
+
+// Домашняя работа
+Console.WriteLine("Введите ваше имя");
+string name = Console.ReadLine();
+Console.WriteLine($"Ваше имя {name}");
