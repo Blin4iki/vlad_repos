9284f483f728381fb1dcf7d3a933dcafd98371ef
Blin4iki <vladdexb@gmail.com>
2024-03-21 20:42:01 +0300

    y

--- проба/проба/Первое занятие дз.cs
+++ проба/проба/Первое занятие дз.cs
@@ -1,45 +1,49 @@
-﻿//Console.WriteLine("Введите Имя");
-//string name= Console.ReadLine();
-//Console.WriteLine("Введите Фамилию");
-//string surname = Console.ReadLine();
-//Console.WriteLine( surname);
-//Console.WriteLine( $"ваше имя {name}");
+﻿////Console.WriteLine("Введите Имя");
+////string name= Console.ReadLine();
+////Console.WriteLine("Введите Фамилию");
+////string surname = Console.ReadLine();
+////Console.WriteLine( surname);
+////Console.WriteLine( $"ваше имя {name}");
 
-//Console.WriteLine("Введите первое число");
-//int number1 = int.Parse(Console.ReadLine());
-//Console.WriteLine("Введите второе число");
-//int number2 = int.Parse(Console.ReadLine());
-//Console.WriteLine(number1 + number2);
+////Console.WriteLine("Введите первое число");
+////int number1 = int.Parse(Console.ReadLine());
+////Console.WriteLine("Введите второе число");
+////int number2 = int.Parse(Console.ReadLine());
+////Console.WriteLine(number1 + number2);
 
-//Console.WriteLine("Введите первое число");
-//int number1 = int.Parse(Console.ReadLine());
-//Console.WriteLine($"{number1 /2 }");
+////Console.WriteLine("Введите первое число");
+////int number1 = int.Parse(Console.ReadLine());
+////Console.WriteLine($"{number1 /2 }");
 
-//Console.WriteLine("Мы имеем прямоугольный треугольник ABC");
-//Console.WriteLine("Надо найтигипотенузу BC");
-//Console.WriteLine("Введите AB");
-//int AB = int.Parse(Console.ReadLine());
-//Console.WriteLine("Введите AC");
-//int AC = int.Parse(Console.ReadLine());
-//Console.WriteLine($"{Math.Sqrt(AB * AB + AC * AC)}");
+////Console.WriteLine("Мы имеем прямоугольный треугольник ABC");
+////Console.WriteLine("Надо найтигипотенузу BC");
+////Console.WriteLine("Введите AB");
+////int AB = int.Parse(Console.ReadLine());
+////Console.WriteLine("Введите AC");
+////int AC = int.Parse(Console.ReadLine());
+////Console.WriteLine($"{Math.Sqrt(AB * AB + AC * AC)}");
 
 
-// Домашняя работа
-Console.WriteLine("Введите ваше имя");
-string name = Console.ReadLine();
-Console.WriteLine($"Ваше имя {name}");
-Console.WriteLine($"Ваше имя {name}");
-
-sdfsdfsdf
+//// Домашняя работа
+//<<<<<<< Updated upstream
 //Console.WriteLine("Введите ваше имя");
 //string name = Console.ReadLine();
 //Console.WriteLine($"Ваше имя {name}");
 //Console.WriteLine($"Ваше имя {name}");
-//Console.WriteLine($"Ваше имя {name}");
-//Console.WriteLine($"Ваше имя {name}");
-//Console.WriteLine($"Ваше имя {name}");
-//Console.WriteLine($"Ваше имя {name}");
 
-double a = 3,141592653
-double b = 2,7182818284590452
-Console.WriteLine($"{a}{b}");
+//sdfsdfsdf
+//=======
+////Console.WriteLine("Введите ваше имя");
+////string name = Console.ReadLine();
+////Console.WriteLine($"Ваше имя {name}");
+////Console.WriteLine($"Ваше имя {name}");
+////Console.WriteLine($"Ваше имя {name}");
+////Console.WriteLine($"Ваше имя {name}");
+////Console.WriteLine($"Ваше имя {name}");
+////Console.WriteLine($"Ваше имя {name}");
+
+double a = 3.141592653;
+decimal b = 2.7182818284590452m;
+Console.WriteLine(a);
+Console.WriteLine(b);
+
